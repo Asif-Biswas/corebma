abd224300425fd2ce5dac1da91d06863c1c54673
Asif Biswas <asifbiswas85@gmail.com>
2023-09-03 13:58:37 +0600

    pass

--- main/serializers.py
+++ main/serializers.py
@@ -7,6 +7,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}}
+
 
 
 class ToDoListSerializer(serializers.ModelSerializer):
